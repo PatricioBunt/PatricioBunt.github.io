49708a550f94579203339ca8acff4fd4b124bb5e
PatricioBunt <47674731+PatricioBunt@users.noreply.github.com>
2023-08-17 01:30:12 -0500

    test mobile

--- js/main.js
+++ js/main.js
@@ -2,14 +2,10 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'orbitcontrols';
 import { GLTFLoader } from 'gltfloader';
-import { OutlineEffect } from 'outlineeffect';
-import { AsciiEffect } from 'ascii';
 
 
-// Rest of the code stays the same
 // Global variables
-let viewmode = false;
-var scene, camera, renderer, controls, outlineEffect;
+var scene, camera, renderer;
 var raycaster = new THREE.Raycaster();
 var mouse = new THREE.Vector2();
 var models = [];
@@ -18,7 +14,12 @@ var targetRotationY = 0;
 var targetRotationX = 0;
 var smoothingFactor = 0.05; // Adjust this value to change the smoothing effect
 
-var text = [{pos: new THREE.Vector3(-3,1.2,0), text: "Home"},{pos:new THREE.Vector3(7.5,2,0), text: "About"},{pos: new THREE.Vector3(-1.7,-6.5,0), text: "Experience"},{pos:new THREE.Vector3(12,-6.3,0),text: "Contact"},]
+var text = [
+             {pos: new THREE.Vector3(-5,2.2,0), text: "HOME", rot:336, b:true}
+            ,{pos:new THREE.Vector3(2.5,3.3,0), text: "ABOUT", rot:56, b:true}
+            ,{pos: new THREE.Vector3(-2.3,-5.1,0), text: "EXPERIENCE", rot:9, b:true}
+            ,{pos:new THREE.Vector3(7.3,-4.1,0),text: "CONTACT", rot:345, b:true}
+            ]
 
 // Initialize the scene
 function init() {
@@ -26,13 +27,8 @@ function init() {
     scene.background = new THREE.Color( '#4287f5');
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     renderer = new THREE.WebGLRenderer();
-    outlineEffect = new OutlineEffect(renderer,{
-        	defaultThickness: 100000,
-        	defaultAlpha: 0.0,
-        	defaultKeepAlive: true // keeps outline material in cache even if material is removed from scene
-        } );
-    outlineEffect.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(outlineEffect.domElement);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
     
 
     // Add lights
@@ -60,26 +56,26 @@ function init() {
     window.addEventListener('mousemove', handleMove, false);
     window.addEventListener('touchmove', handleMove, false);
     window.addEventListener('click', handleClick, false);
-    window.addEventListener('touchend', handleClick, false);
+    window.addEventListener('touchstart', handleClick, false);
+    document.addEventListener('touchend', resetCameraRotation, false);
     window.addEventListener( 'resize', onWindowResize );
+
     // Check if the browser supports the DeviceOrientationEvent
-        if (window.DeviceOrientationEvent) {
-            window.addEventListener('deviceorientation', function(event) {
-                // Get the alpha (compass direction), beta (tilt front-to-back), and gamma (tilt left-to-right) values
-                var alpha = event.alpha;
-                var beta = event.beta;
-                var gamma = event.gamma;
+    if (window.DeviceOrientationEvent) {
+        window.addEventListener('deviceorientation', function(event) {
+            // Get the alpha (compass direction), beta (tilt front-to-back), and gamma (tilt left-to-right) values
+            var alpha = event.alpha;
+            var beta = event.beta;
+            var gamma = event.gamma;
 
-                // Convert these to values that make sense for your scene
-                var xRotation = THREE.MathUtils.degToRad(beta); // Tilt front-to-back
-                var yRotation = THREE.MathUtils.degToRad(gamma); // Tilt left-to-right
+            var xRotation = THREE.MathUtils.degToRad(beta); // Tilt front-to-back
+            var yRotation = THREE.MathUtils.degToRad(gamma); // Tilt left-to-right
 
-                // Apply these rotations to your camera or any other object in your scene
-                camera.rotation.set(xRotation, yRotation, 0);
-            });
-        } else {
-            console.warn('DeviceOrientationEvent is not supported on this device');
-        }
+            camera.rotation.set(xRotation, yRotation, 0);
+        });
+    } else {
+        console.warn('DeviceOrientationEvent is not supported on this device');
+    }
     
     camera.position.x = 1.388781733064856
     camera.position.y = 0;
@@ -89,10 +85,10 @@ function init() {
     camera.rotation.z = 0;
     var controls = new OrbitControls(camera, renderer.domElement);
     animate();
-    fitToScreen(models.map(m => m.model));
+    fitToScreen();
 }
 
-function fitToScreen(objects) {
+function fitToScreen() {
     
     var minBounds = new THREE.Vector3(-10, -8.5, -8);
     var maxBounds = new THREE.Vector3(10, 8.5, 2);
@@ -106,8 +102,8 @@ function fitToScreen(objects) {
     var factor = 1.2;
     var cameraZ = maxDim / (2 * Math.tan((camera.fov / 2) * (Math.PI / 180)));
 
-    camera.position.z = center.z + cameraZ * factor;
-
+    camera.fov = cameraZ / camera.position.z * camera.fov * factor;
+    console.log(camera.fov);
 }
 
 
@@ -117,7 +113,6 @@ function onWindowResize() {
     camera.updateProjectionMatrix();
 
     renderer.setSize( window.innerWidth, window.innerHeight );
-    outlineEffect.setSize( window.innerWidth, window.innerHeight );
 
 }
 // Load a model with given path and position
@@ -125,33 +120,72 @@ function loadModel(loader, path, position, textIndex) {
     loader.load(path, function (gltf) {
         var model = gltf.scene;
         model.position.set(position.x, position.y, position.z);
-        model.rotation.y = -90.1;
-        model.rotation.x = 12.25;
+        model.rotation.y = THREE.MathUtils.degToRad(-122);
+        model.rotation.x = THREE.MathUtils.degToRad(-20);
         
         scene.add(model);
-        var sprite = makeTextSprite(text[textIndex].text);
-        sprite.position.set(text[textIndex].pos.x,text[textIndex].pos.y,text[textIndex].pos.z);
+        var sprite = makeTextSprite(text[textIndex].text,text[textIndex].rot, text[textIndex].b);
+        sprite.position.set(text[textIndex].pos.x ,text[textIndex].pos.y,text[textIndex].pos.z);
+        sprite.visible = false;
         scene.add(sprite);
 
         models.push({ model: model, text: sprite }); // Only add the model once, with the associated text sprite
     });
 }
-// Create a text sprite
-function makeTextSprite(message) {
+
+function makeTextSprite(message, rotationAngle, black) {
+    var tempCanvas = document.createElement('canvas');
+    var tempContext = tempCanvas.getContext('2d');
+    var fontSize = 200;
+    tempContext.font = fontSize + "px Arial";
+
+    var textWidth = tempContext.measureText(message).width;
+    var textHeight = fontSize + 20; // Extra 20 pixels for padding
+
+    // Calculate the size of the bounding box after rotation
+    var cosAngle = Math.cos(rotationAngle);
+    var sinAngle = Math.sin(rotationAngle);
+    var rotatedWidth = Math.abs(cosAngle * textWidth) + Math.abs(sinAngle * textHeight)+100;
+    var rotatedHeight = Math.abs(sinAngle * textWidth) + Math.abs(cosAngle * textHeight)+100;
+
+    // Create the actual canvas with the calculated size
     var canvas = document.createElement('canvas');
+    canvas.width = rotatedWidth;
+    canvas.height = rotatedHeight;
     var context = canvas.getContext('2d');
-    context.font = "20px Arial";
-    context.fillText(message, 0, 20);
+    context.font = fontSize + "px Verdana";
+    if(black){
+        context.fillStyle = "rgba(0, 0, 0, 1)";
+    }else{
+        context.fillStyle = "rgba(255, 255, 255, 1)";
+    }
+    context.textBaseline = 'top'; // Set baseline to top
+
+    // Translate to the center of the canvas
+    context.translate(rotatedWidth / 2, rotatedHeight / 2);
+
+    // Rotate the canvas
+    context.rotate(THREE.MathUtils.degToRad(rotationAngle));
+
+    // Translate back to the original position
+    context.translate(-textWidth / 2, -fontSize / 2); // Y-coordinate adjusted to center the text vertically
+
+    context.fillText(message, 0, 0); // Y-coordinate is 0 now
 
     var texture = new THREE.Texture(canvas);
     texture.needsUpdate = true;
 
     var spriteMaterial = new THREE.SpriteMaterial({ map: texture });
     var sprite = new THREE.Sprite(spriteMaterial);
-    sprite.scale.set(10, 5, 1);
+
+    var scale = 0.004; // Adjust the scale as needed
+    sprite.scale.set(rotatedWidth * scale, rotatedHeight * scale, 1);
 
     return sprite;
 }
+
+
+
 
 // Mouse click event handler
 function handleClick(event) {
@@ -253,12 +287,16 @@ function smoothCameraRotation() {
     camera.rotation.y += (targetRotationY - camera.rotation.y) * smoothingFactor;
     camera.rotation.x += (targetRotationX - camera.rotation.x) * smoothingFactor;
 }
+function resetCameraRotation() {
+    targetRotationX = defaultRotationX;
+    targetRotationY = defaultRotationY;
+}
 // Animate the scene
 function animate() {
     requestAnimationFrame(animate);
     smoothCameraRotation();
 
-    outlineEffect.render(scene, camera);
+    renderer.render(scene, camera);
 }
 
 // Start the scene
