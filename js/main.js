2e91b774ba94144b016807d6f952e53b4d6fe10d
PatricioBunt <47674731+PatricioBunt@users.noreply.github.com>
2023-08-17 00:42:07 -0500

    test mobile

--- js/main.js
+++ js/main.js
@@ -13,11 +13,12 @@ var scene, camera, renderer, controls, outlineEffect;
 var raycaster = new THREE.Raycaster();
 var mouse = new THREE.Vector2();
 var models = [];
-var maxCameraAngle = 0.52; // 30 degrees in radians
+var maxCameraAngle = 0.26; // 30 degrees in radians
 var targetRotationY = 0;
 var targetRotationX = 0;
 var smoothingFactor = 0.05; // Adjust this value to change the smoothing effect
 
+var text = [{pos: new THREE.Vector3(-3,1.2,0), text: "Home"},{pos:new THREE.Vector3(7.5,2,0), text: "About"},{pos: new THREE.Vector3(-1.7,-6.5,0), text: "Experience"},{pos:new THREE.Vector3(12,-6.3,0),text: "Contact"},]
 
 // Initialize the scene
 function init() {
@@ -56,19 +57,57 @@ function init() {
     loadModel(loader, 'models/model4.glb', new THREE.Vector3(0, 2, 0), 3);
 
     // Event listener for mouse click
-    window.addEventListener('mousemove', onMouseMove, false);
-    window.addEventListener('click', onMouseClick, false);
+    window.addEventListener('mousemove', handleMove, false);
+    window.addEventListener('touchmove', handleMove, false);
+    window.addEventListener('click', handleClick, false);
+    window.addEventListener('touchend', handleClick, false);
     window.addEventListener( 'resize', onWindowResize );
+    // Check if the browser supports the DeviceOrientationEvent
+        if (window.DeviceOrientationEvent) {
+            window.addEventListener('deviceorientation', function(event) {
+                // Get the alpha (compass direction), beta (tilt front-to-back), and gamma (tilt left-to-right) values
+                var alpha = event.alpha;
+                var beta = event.beta;
+                var gamma = event.gamma;
 
+                // Convert these to values that make sense for your scene
+                var xRotation = THREE.MathUtils.degToRad(beta); // Tilt front-to-back
+                var yRotation = THREE.MathUtils.degToRad(gamma); // Tilt left-to-right
+
+                // Apply these rotations to your camera or any other object in your scene
+                camera.rotation.set(xRotation, yRotation, 0);
+            });
+        } else {
+            console.warn('DeviceOrientationEvent is not supported on this device');
+        }
     
-    camera.position.x = 2.388781733064856
-    camera.position.y = 0.227376770315975;
+    camera.position.x = 1.388781733064856
+    camera.position.y = 0;
     camera.position.z = 11.50292550000304;
     camera.rotation.x = 0;
     camera.rotation.y = 0;
     camera.rotation.z = 0;
-
+    var controls = new OrbitControls(camera, renderer.domElement);
     animate();
+    fitToScreen(models.map(m => m.model));
+}
+
+function fitToScreen(objects) {
+    
+    var minBounds = new THREE.Vector3(-10, -8.5, -8);
+    var maxBounds = new THREE.Vector3(10, 8.5, 2);
+    
+    // Create a bounding box with the manually determined bounds
+    var boundingBox = new THREE.Box3(minBounds, maxBounds);
+    var size = boundingBox.getSize(new THREE.Vector3());
+    var maxDim = Math.max(size.x, size.y, size.z);
+    var center = boundingBox.getCenter(new THREE.Vector3());
+    
+    var factor = 1.2;
+    var cameraZ = maxDim / (2 * Math.tan((camera.fov / 2) * (Math.PI / 180)));
+
+    camera.position.z = center.z + cameraZ * factor;
+
 }
 
 
@@ -81,7 +120,6 @@ function onWindowResize() {
     outlineEffect.setSize( window.innerWidth, window.innerHeight );
 
 }
-var text = [{pos: new THREE.Vector3(-3,1.2,0), text: "Home"},{pos:new THREE.Vector3(7.5,2,0), text: "About"},{pos: new THREE.Vector3(-1.7,-6.5,0), text: "Experience"},{pos:new THREE.Vector3(12,-6.3,0),text: "Contact"},]
 // Load a model with given path and position
 function loadModel(loader, path, position, textIndex) {
     loader.load(path, function (gltf) {
@@ -116,7 +154,12 @@ function makeTextSprite(message) {
 }
 
 // Mouse click event handler
-function onMouseClick(event) {
+function handleClick(event) {
+    if (event.touches) {
+        // For touch events, use the first touch
+        event.clientX = event.touches[0].clientX;
+        event.clientY = event.touches[0].clientY;
+    }
     mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
     mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
     raycaster.setFromCamera(mouse, camera);
@@ -124,17 +167,39 @@ function onMouseClick(event) {
     var intersects = raycaster.intersectObjects(models.map(m => m.model));
 
     if (intersects.length > 0) {
-        var intersection = intersects[0];
+        var intersection = intersects[0].object;
 
-        models.forEach(m => {
-            if (m.model === intersection.object) {
-                window.location.href = "/page" + m.text.text;
-            }
-        });
+        // Find the top-level parent model of the intersected object
+        var parentModel = intersection;
+        while (parentModel.parent !== scene) {
+            parentModel = parentModel.parent;
+        }
+
+        // Find the corresponding entry in the models array
+        let foundModel = models.find(m => m.model === parentModel);
+        if (foundModel) {
+            // Traverse the parent model and set emissivity for all child meshes
+            parentModel.traverse(child => {
+                if (child.isMesh) {
+                    child.material.emissive.set(0x666666);
+                }
+            });
+
+            window.location.href = "/page" + foundModel.text;
+        } else {
+            console.warn('Parent model not found in models array. Intersection object:', intersection);
+        }
+
     }
 }
 // Mouse move event handler for hover effect
-function onMouseMove(event) {
+function handleMove(event) {
+
+    if (event.touches) {
+        // For touch events, use the first touch
+        event.clientX = event.touches[0].clientX;
+        event.clientY = event.touches[0].clientY;
+    }
 
 
     mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
